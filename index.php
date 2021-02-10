5dbdae053836f661180149b08f068cba170a9d90
Jonas Born <jonas.born@yahoo.de>
2021-02-10 21:24:31 +0100

    Added fixes for chrome

--- index.php
+++ index.php
@@ -3,17 +3,22 @@
 require_once __DIR__ . "/IdGenerator.php";
 $dir = __DIR__ . "/pastes";
 
+
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    echo "POST";
     require_once "write.php";
     exit();
+} else if (isset($_GET["id"])) {
+
+    require_once "read.php";
+    exit();
+
 } else {
-    if (isset($_GET["id"])) {
-        require_once "read.php";
-        exit();
-    }
+    echo file_get_contents("template.html");
 }
 
-echo file_get_contents("index.html");
+
 
 ?>
 
